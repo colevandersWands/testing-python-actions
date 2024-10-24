e61a4023ad9f3935a1b7fccf4c0c55c0d0568f7d
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2024-10-24 18:37:43 -0400

    pass ruff (so linting), warnings from pylint

--- solutions/miles_to_km.py
+++ solutions/miles_to_km.py
@@ -17,5 +17,5 @@ def miles_to_km(miles: float) -> float:
     if isinstance(miles, (float, int, str)):
         km = miles * 1.60934
         return round(km, 2)
-    else:
-        raise TypeError(f"Input must be a float or int. Input was {type(miles)}")
+
+    raise TypeError(f"Input must be a float or int. Input was {type(miles)}")
--- tests/__init__.py
+++
@@ -1,3 +0,0 @@
-"""module docstring"""
-
-import unittest
