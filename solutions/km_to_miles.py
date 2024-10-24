3c78bfecd5f8817ca2f682e4cb677ab7bb201014
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2024-10-24 16:19:12 -0400

    fix errors

--- solutions/km_to_miles.py
+++ solutions/km_to_miles.py
@@ -14,10 +14,6 @@ def km_to_miles(km: float) -> float:
     if isinstance(km, float) or isinstance(km, int) or isinstance(km, str):
         miles = km / 1.60934
 
-
-
-
-        
         return round(miles, 2)
     else:
         raise TypeError(f"Input must be a float or int. Input was {type(km)}")
--- tests/test_km_to_miles.py
+++ tests/test_km_to_miles.py
@@ -5,8 +5,7 @@ from solutions.km_to_miles import km_to_miles
 
 class Test(unittest.TestCase):
     def test_km_to_miles(self):
-        # self.assertEqual(km_to_miles(1), 0.62)
-        self.assertEqual(km_to_miles(1), 62)
+        self.assertEqual(km_to_miles(1), 0.62)
 
     def test_km_to_miles_dict_type(self):
         with self.assertRaises(TypeError):
